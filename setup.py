796454dfd5d0e39d66199017f60279be893122ad
kotfu <kotfu@kotfu.net>
2018-10-14 18:15:05 -0600

    Update classifiers to include python 3.7

--- setup.py
+++ setup.py
@@ -39,6 +39,7 @@ setuptools.setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
 
     # dependencies for development and testing
